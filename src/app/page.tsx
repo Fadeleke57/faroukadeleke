fcd5c84cc7cef20b673ffa826ca829dc30eadb61
fadeleke57@gmail.com <fadeleke57@gmail.com>
2024-01-03 17:52:32 -0500

    Component files and imports to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Bio from './components/Bio'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Landing from './components/Landing'
+import Navbar from './components/Navbar'
+import Projects from './components/Projects'
 
 export default function Home() {
   return (
