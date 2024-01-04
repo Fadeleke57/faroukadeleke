a91a7e47129acd24b7e9ad88ad3c80e5fa0e4ba9
fadeleke57@gmail.com <fadeleke57@gmail.com>
2024-01-03 23:16:31 -0500

    Navbar, landing page skeleton, fade dependendies, icon dependences

--- src/app/components/Landing.jsx
+++ src/app/components/Landing.jsx
@@ -1,8 +1,27 @@
+'use client'
 import styles from '../styles/Landing.module.css'
+import Image from 'next/image'
+import profilePic from '../../../public/profilePic.jpg'
+import { Fade } from "react-awesome-reveal";
 
 function Landing() {
   return (
-    <div></div>
+    <section className={styles.landing}>
+      <div className={styles.contentWrapper}>
+
+      <div className={styles.landingHeader}>
+          <div>
+            <Fade cascade>
+            <p>Hello! I'm..</p>
+            <h1>Farouk Adeleke</h1>
+            <h3>Student at <span className={styles.bostonUniversity}>Boston University</span></h3>
+            <h3>Freelance <span className={styles.webDev}>Web Devlopment</span></h3>
+            </Fade>
+          </div>
+      </div>
+
+      </div>
+    </section>
   )
 }
 
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,8 +1,51 @@
+'use client'
 import styles from '../styles/Navbar.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { useState, useEffect } from 'react'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import Search from './Search'
+
 
 function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [nav, setNav] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    function activateNav() {
+      let scrollPosition = window.pageYOffset
+      if(scrollPosition > 200) {
+        setNav(true)
+      } else if (scrollPosition < 10) {
+        setNav(false)
+      }
+    }
+    window.addEventListener("scroll", activateNav)
+  })
+
+  const menuToggle = () => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
-    <div></div>
+    <nav className={styles.navbar}>
+
+      <div className={styles.container}>
+        <div className={styles.menu}>
+
+          <FontAwesomeIcon icon={faGithub} className={styles.icon}/>
+          <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />  
+      
+          <Search/>
+        
+        </div>
+
+      </div>
+
+    </nav>
   )
 }
 
