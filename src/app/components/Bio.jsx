0d1dbcef936f108bf78f6369ea838aa9a71c8be9
fadeleke57@gmail.com <fadeleke57@gmail.com>
2024-01-03 19:43:32 -0500

    Updates to import statements in all components

--- src/app/components/Bio.jsx
+++ src/app/components/Bio.jsx
@@ -1,4 +1,4 @@
-import styles from '../styles/bio.module.css'
+import styles from '../styles/Bio.module.css'
 
 function Bio() {
   return (
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import styles from '../styles/navbar.module.css'
+import styles from '../styles/Navbar.module.css'
 
 function Navbar() {
   return (
